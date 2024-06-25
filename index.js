aa3145f8651d388e81a80e90ce48247d45954b4c
Harmony <harmonymukolwe@gmail.com>
2024-06-26 01:49:17 +0300

    Executes functions

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(funActivity='roller-skate') {
+  return `This Saturday, I want to ${funActivity}!`
+}
+
+const mondayWork = function(work='go to the office') {
+  return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(str='*') {
+  return function(param='special') {
+    return `You are ${str}${param}${str}!`
+  }
+}
